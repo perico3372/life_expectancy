6f8849a459fa2baa2809e4cb8504d285d3e4208d
perico3372 <117478891+perico3372@users.noreply.github.com>
2023-06-19 15:08:14 -0300

    Update recommendation_streamlit.py

--- recommendation_streamlit.py
+++ recommendation_streamlit.py
@@ -3,13 +3,27 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.impute import SimpleImputer
 import streamlit as st
 from collections import Counter
+from sqlalchemy import create_engine
+
+server = 'database1234.database.windows.net'
+database = 'DataBase'
+username = 'administrador'
+password = '42757875P.'
+driver = 'ODBC Driver 17 for SQL Server'
+connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}'
+
+engine = create_engine(connection_string)
+query = 'SELECT * FROM [dbo].[Data-Bank]'
+
+data = pd.read_sql(query, engine)
+
 
 
 # Título de la aplicación
 st.title("ESPERANZA DE VIDA AL NACER")
 
 # Cargar los datos en un DataFrame de pandas
-data = pd.read_csv('https://raw.githubusercontent.com/perico3372/life_expectancy/main/DATA_PF_IV.csv')
+#data = pd.read_csv('https://raw.githubusercontent.com/perico3372/life_expectancy/main/DATA_PF_IV.csv')
 #data = pd.read_csv('data.csv')
 
 # Seleccionar los indicadores como características
@@ -60,21 +74,25 @@ def get_recommendations(country_index, n_recommendations=3):
     valid_indices = [i for i in indicator_indices if i < len(features)]
     return valid_indices
 
+# Mostrar la data con opción para seleccionar una muestra aleatoria
+show_random_sample = st.checkbox("Mostrar una muestra aleatoria")
+if show_random_sample:
+    # Obtener el tamaño de la muestra deseada
+    sample_size = st.number_input("Seleccione el tamaño de la muestra", min_value=1, max_value=len(data), value=5, step=1)
 
-# Mostrar la data con opción para seleccionar los 5 primeros o 5 últimos registros
-show_first = st.checkbox("Mostrar los 5 primeros registros")
-show_last = st.checkbox("Mostrar los 5 últimos registros")
+    # Obtener una muestra aleatoria de los datos
+    random_sample = data.sample(n=sample_size)
 
-if show_first:
-    st.write("Primeros 5 registros:")
-    st.dataframe(data.head(5))
-if show_last:
-    st.write("Últimos 5 registros:")
-    st.dataframe(data.tail(5))
-
+    # Mostrar la muestra aleatoria
+    st.write(f"Muestra aleatoria de {sample_size} registros:")
+    st.dataframe(random_sample)
+    
 # Subtítulo - Series de tiempo
 st.subheader("Modelo KNN")
 
+# Obtener la cantidad de indicadores a mostrar
+num_indicators = st.number_input("Seleccione la cantidad de indicadores a mostrar", min_value=1, max_value=len(features), value=7)
+    
 # Generar los indicadores más influyentes al hacer clic en un botón
 if st.button("Generar indicadores más influyentes"):
     # Crear una lista para almacenar todas las recomendaciones de indicadores
@@ -88,12 +106,14 @@ if st.button("Generar indicadores más influyentes"):
     # Contar la frecuencia de cada indicador recomendado
     counter = Counter(all_recommended_indicators)
 
-    # Obtener los indicadores más influyentes
-    top_indicators = counter.most_common(7)
 
-    # Mostrar los indicadores más influyentes
+
+    # Obtener los indicadores más influyentes
+    top_indicators = counter.most_common(num_indicators)
+
+    # Mostrar los indicadores seleccionados
     st.title("Indicadores más influyentes para la esperanza de vida")
-    st.write("Los siete indicadores más influyentes para la esperanza de vida:")
+    st.write(f"Los {num_indicators} indicadores más influyentes para la esperanza de vida:")
 
     for indicator, count in top_indicators:
         st.write(f"- {indicator}")
